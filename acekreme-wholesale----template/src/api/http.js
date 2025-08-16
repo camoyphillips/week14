2aec78ce6dd5456b64a73fdb9e871e2e0037f968
Camoy Phillips <n01496384@humber.ca>
2025-08-15 23:45:00 -0400

    Acekreme.ca

--- acekreme-wholesale----template/src/api/http.js
+++ acekreme-wholesale----template/src/api/http.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // Uses Vite proxy in dev, or VITE_API_BASE in prod
 const http = axios.create({
   baseURL: import.meta.env.VITE_API_BASE || "",
-  withCredentials: true, // if you need session cookie for /logout or auth
+  withCredentials: true, 
 });
 
 export default http;
